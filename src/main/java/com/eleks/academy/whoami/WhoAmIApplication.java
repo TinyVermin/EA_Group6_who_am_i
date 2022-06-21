3ed8e1472b1c67b1d37e784be2eae54fdef8e903
Dmitry <dmitrymishnev@gmail.com>
2022-06-21 21:46:32 +0300

    EAG6-16: changed  swagger by org.springdoc on swagger by io.springfox

--- src/main/java/com/eleks/academy/whoami/WhoAmIApplication.java
+++ src/main/java/com/eleks/academy/whoami/WhoAmIApplication.java
@@ -2,8 +2,10 @@ package com.eleks.academy.whoami;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+@EnableWebMvc
 public class WhoAmIApplication {
 
     public static void main(String[] args) {
